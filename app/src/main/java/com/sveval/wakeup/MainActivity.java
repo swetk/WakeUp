80ced4d548005eeedfa85fd6e150498a8d2b4f53
swetk <zigasvet7@gmail.com>
2016-12-16 12:24:38 +0100

    add items to string array

--- app/src/main/java/com/sveval/wakeup/MainActivity.java
+++ app/src/main/java/com/sveval/wakeup/MainActivity.java
@@ -1,13 +1,27 @@
 package com.sveval.wakeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void newDestination(View view) {
+        Intent intent = new Intent(this, UrediDestinacijo.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
+++ app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
@@ -2,12 +2,26 @@ package com.sveval.wakeup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class UrediDestinacijo extends AppCompatActivity {
 
+    AutoCompleteTextView autoCompleteTextView;
+    String[] imenaPostaj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uredi_destinacijo);
+
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.vstopnaPostaja);
+        imenaPostaj = getResources().getStringArray(R.array.postaje);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
+        autoCompleteTextView.setAdapter(adapter);
+
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.iztopnaPostaja);
+        imenaPostaj = getResources().getStringArray(R.array.postaje);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
+        autoCompleteTextView.setAdapter(adapter2);
     }
 }
