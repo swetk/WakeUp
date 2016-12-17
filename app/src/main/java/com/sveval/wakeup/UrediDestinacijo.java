3167a94dea61f403412f24c115e3e89db7ccd3ea
swetk <zigasvet7@gmail.com>
2016-12-17 14:34:14 +0100

    activity uredi destinacijo - manjka buttonKoncaj onClick koncajDestinacijo

--- app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
+++ app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
@@ -1,6 +1,8 @@
 package com.sveval.wakeup;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,22 +16,23 @@ import java.util.List;
 
 public class UrediDestinacijo extends AppCompatActivity {
 
-    AutoCompleteTextView autoCompleteTextView;
+    AutoCompleteTextView autoCompleteTextViewVstopna;
+    AutoCompleteTextView autoCompleteTextViewIztop;
     String[] imenaPostaj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uredi_destinacijo);
 
-        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.vstopnaPostaja);
+        autoCompleteTextViewVstopna = (AutoCompleteTextView) findViewById(R.id.vstopnaPostaja);
         imenaPostaj = getResources().getStringArray(R.array.postaje);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
-        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextViewVstopna.setAdapter(adapter);
 
-        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.iztopnaPostaja);
+        autoCompleteTextViewIztop = (AutoCompleteTextView) findViewById(R.id.iztopnaPostaja);
         imenaPostaj = getResources().getStringArray(R.array.postaje);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
-        autoCompleteTextView.setAdapter(adapter2);
+        autoCompleteTextViewVstopna.setAdapter(adapter2);
 
     }
 
@@ -39,6 +42,20 @@ public class UrediDestinacijo extends AppCompatActivity {
     }
 
     public void koncajDestinacijo (View view) {
+        setVstopnaPostaja(this, autoCompleteTextViewVstopna.getText().toString());
+        autoCompleteTextViewIztop.getText(getVstopnaPostaja(this));
+    }
+
+    public static void setVstopnaPostaja(Context context, String vstopnaPostaja) {
+        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewVstopna", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("autoCompleteTextViewVstopna", vstopnaPostaja);
+        editor.commit();
+    }
+
+    public static String getVstopnaPostaja(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("vstopnaPostaja", 0);
+        return prefs.getString("vstopnaPostaja", "");
 
     }
 }
