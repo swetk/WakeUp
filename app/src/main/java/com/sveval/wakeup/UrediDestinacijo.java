701e04847fa66e116e119eb013577bf025fcc7d3
timv5 <tim.valencic@gmail.com>
2016-12-17 14:34:59 +0100

    dodajanje opomnika na mainActivity

--- app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
+++ app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
@@ -1,16 +1,23 @@
 package com.sveval.wakeup;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import android.view.ViewGroup.LayoutParams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class UrediDestinacijo extends AppCompatActivity {
 
@@ -39,6 +46,14 @@ public class UrediDestinacijo extends AppCompatActivity {
     }
 
     public void koncajDestinacijo (View view) {
-
+                Random r = new Random();
+                int id = (r.nextInt(100)+1);    //generira random id
+                RelativeLayout layout = (RelativeLayout) findViewById(R.id.activity_main);
+                Button createdButton = new Button(this);
+                createdButton.setText("Ljubljana-Maribor");
+                createdButton.setId(id);
+                layout.addView(createdButton);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
