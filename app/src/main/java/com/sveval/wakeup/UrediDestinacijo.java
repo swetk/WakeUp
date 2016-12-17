02f1798b0c976d92a68ca2b25c4b4a741cefcb87
swetk <zigasvet7@gmail.com>
2016-12-17 12:59:52 +0100

    activity uredi destinacijo - manjka buttonKoncaj onClick

--- app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
+++ app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
@@ -1,9 +1,16 @@
 package com.sveval.wakeup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UrediDestinacijo extends AppCompatActivity {
 
@@ -23,5 +30,11 @@ public class UrediDestinacijo extends AppCompatActivity {
         imenaPostaj = getResources().getStringArray(R.array.postaje);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
         autoCompleteTextView.setAdapter(adapter2);
+
+    }
+
+    public void getBackMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
