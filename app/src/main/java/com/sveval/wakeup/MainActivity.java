f83dd48f6e9b86fc46c50219450f8c05d64fdff8
swetk <zigasvet7@gmail.com>
2016-12-29 14:34:42 +0100

    pomnenje programsko dodajanje gumbov

--- app/src/main/java/com/sveval/wakeup/MainActivity.java
+++ app/src/main/java/com/sveval/wakeup/MainActivity.java
@@ -1,31 +1,22 @@
 package com.sveval.wakeup;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int buttonCount;
-    public static ListView lwButtons;
-    public static Button button;
+    public static ArrayList<String> items = new ArrayList<>();
+    public static ListView listt;
+    Button btn;
     private Toolbar toolbar;
-    public static ArrayList<String> items;
-    public static int k,l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +26,14 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
-        ListView listt = (ListView) findViewById(R.id.listView);
+        TinyDB tinydb = new TinyDB(this);
+        items = tinydb.getListString("ww");
+
+        System.out.println(items);
+
+        listt = (ListView) findViewById(R.id.listView);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.destination_list, R.id.buttonDestionation, items);
         listt.setAdapter(adapter);
-
-
-        //DestinationListAdapter adapter = new DestinationListAdapter(AddNewPerson.this, R.layout.destination_list, editPersonData.getAtomPayments());
-        //ListView atomPaysListView = (ListView)findViewById(R.id.listView);
-        //atomPaysListView.setAdapter(adapter);
-
-        //llButtons.addView(button);
-
-        /*Button btnAdd = (Button) findViewById(R.id.btn_add);
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                doAddButton();
-            }
-        });
-        llButtons = (LinearLayout) findViewById(R.id.ll_buttons);*/
-
     }
 
     @Override
@@ -88,17 +67,19 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 
-    public void doAddButton(View view) {
-        lwButtons = (ListView) findViewById(R.id.listView);
-        Button button = new Button(this);
-        button.setText("Button " + ++buttonCount);
-        button.setLayoutParams(new AbsListView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        lwButtons.setAdapter((ListAdapter) button);
-
-    }
-
     public void newDestination(View view) {
         Intent intent = new Intent(MainActivity.this, UrediDestinacijo.class);
         startActivity(intent);
     }
+
+    public void removeDestination(View view) {
+        TinyDB tinydb = new TinyDB(this);
+        items.remove(0);
+        tinydb.putListString("ww", items);
+        listt = (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.destination_list, R.id.buttonDestionation, items);
+        listt.setAdapter(adapter);
+    }
+
 }
+
--- app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
+++ app/src/main/java/com/sveval/wakeup/UrediDestinacijo.java
@@ -3,32 +3,45 @@ package com.sveval.wakeup;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 import static com.sveval.wakeup.MainActivity.items;
-import static com.sveval.wakeup.MainActivity.k;
-import static com.sveval.wakeup.MainActivity.l;
 
 public class UrediDestinacijo extends AppCompatActivity {
 
     AutoCompleteTextView autoCompleteTextViewVstopna;
     AutoCompleteTextView autoCompleteTextViewIztop;
     String[] imenaPostaj;
+
+    public static void setVstopnaPostaja(Context context, String vstopnaPostaja) {
+        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewVstopna", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("autoCompleteTextViewVstopna", vstopnaPostaja);
+        editor.commit();
+    }
+
+    public static String getVstopnaPostaja(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewVstopna", 0);
+        return prefs.getString("autoCompleteTextViewVstopna", "");
+
+    }
+
+    public static void setIztopnaPostaja(Context context, String iztopnaPostaja) {
+        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewIztop", 0);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("autoCompleteTextViewIztop", iztopnaPostaja);
+        editor.commit();
+    }
+
+    public static String getIztopnaPostaja(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewIztop", 0);
+        return prefs.getString("autoCompleteTextViewIztop", "");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +50,12 @@ public class UrediDestinacijo extends AppCompatActivity {
 
         autoCompleteTextViewVstopna = (AutoCompleteTextView) findViewById(R.id.vstopnaPostaja);
         imenaPostaj = getResources().getStringArray(R.array.postaje);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, imenaPostaj);
         autoCompleteTextViewVstopna.setAdapter(adapter);
 
         autoCompleteTextViewIztop = (AutoCompleteTextView) findViewById(R.id.iztopnaPostaja);
         imenaPostaj = getResources().getStringArray(R.array.postaje);
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,imenaPostaj);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, imenaPostaj);
         autoCompleteTextViewIztop.setAdapter(adapter2);
 
     }
@@ -77,53 +90,27 @@ public class UrediDestinacijo extends AppCompatActivity {
         super.onDestroy();
     }
 
-
-    public void koncajDestinacijo (View view) {
+    public void koncajDestinacijo(View view) {
         //save data
         setVstopnaPostaja(this, autoCompleteTextViewVstopna.getText().toString());
         setIztopnaPostaja(this, autoCompleteTextViewIztop.getText().toString());
         //get data
+        //items = new ArrayList<String>();
         items.add(getVstopnaPostaja(this));
         items.add(getIztopnaPostaja(this));
 
-        ListView listt = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.destination_list, R.id.buttonDestionation, items);
-        listt.setAdapter(adapter);
+        TinyDB tinydb = new TinyDB(this);
+        tinydb.putListString("ww", items);
 
-
+        Intent finish = new Intent(UrediDestinacijo.this, MainActivity.class);
+        startActivity(finish);
+        finish();
 
     }
 
-    public void goBackMain (View view) {
+    public void goBackMain(View view) {
         Intent intent = new Intent(UrediDestinacijo.this, MainActivity.class);
         startActivity(intent);
         finish();
-    }
-
-
-    public static void setVstopnaPostaja(Context context, String vstopnaPostaja) {
-        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewVstopna", 0);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("autoCompleteTextViewVstopna", vstopnaPostaja);
-        editor.commit();
-    }
-
-    public static String getVstopnaPostaja(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewVstopna", 0);
-        return prefs.getString("autoCompleteTextViewVstopna", "");
-
-    }
-
-    public static void setIztopnaPostaja(Context context, String iztopnaPostaja) {
-        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewIztop", 0);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("autoCompleteTextViewIztop", iztopnaPostaja);
-        editor.commit();
-    }
-
-    public static String getIztopnaPostaja(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences("autoCompleteTextViewIztop", 0);
-        return prefs.getString("autoCompleteTextViewIztop", "");
-
     }
 }
